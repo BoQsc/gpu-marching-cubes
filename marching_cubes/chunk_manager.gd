81656546299e8daec9b2408d552de3bf16a75d67
Feldwor <BoQsc@users.noreply.github.com>
2025-12-19 22:07:12 +0200

    Add optimization guide

--- marching_cubes/chunk_manager.gd
+++ marching_cubes/chunk_manager.gd
@@ -1444,7 +1444,6 @@ func complete_generation(coord: Vector3i, result_t: Dictionary, dens_t: RID, res
 
 func _finalize_chunk_creation(item: Dictionary):
 	if item.type == "generation":
-		var start_time = Time.get_ticks_usec()
 		var coord = item.coord
 		
 		# Check if chunk is still wanted
@@ -1459,10 +1458,6 @@ func _finalize_chunk_creation(item: Dictionary):
 		
 		var result_t = create_chunk_node(item.result_t.mesh, item.result_t.shape, chunk_pos, false, chunk_material)
 		var result_w = create_chunk_node(item.result_w.mesh, item.result_w.shape, chunk_pos, true)
-		
-		var duration = (Time.get_ticks_usec() - start_time) / 1000.0
-		if duration > 2.0: # Print if takes longer than 2ms
-			print("Chunk Finalize took: %.2f ms" % duration)
 		
 		var data = ChunkData.new()
 		data.node_terrain = result_t.node if not result_t.is_empty() else null
